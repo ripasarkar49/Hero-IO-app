6c9ce036819752d068c278aa51f6cd04b0c8d856
Ripa Sarkar <ripasarkar49@gmail.com>
2025-11-22 22:05:28 +0600

    all appData set

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import UseApp from "../hooks/UseApp";
+import AppCards from "../components/AppCards";
 
 const App = () => {
+  const { apps, loading, error } = UseApp();
   return (
-    <div>
-      app
+    <div className="w-11/12 mx-auto">
+      <div className="text-center  py-6">
+        <h2 className="font-bold text-3xl">Our All Applications</h2>
+        <p className="text-gray-400">
+          Explore All Apps on the Market developed by us. We code for Millions
+        </p>
+      </div>
+      <div className="flex justify-between py-5 items-center">
+        <h2 className="text-2xl font-semibold">({apps.length}) App Found</h2>
+        <label className="input">
+          <input type="search" placeholder="Search App" />
+        </label>
+      </div>
+      <div className=" grid grid-cols-1 md:grid-cols-2 pb-6 lg:grid-cols-4 gap-5 ">
+        {apps.map((app) => (
+          <AppCards key={app.id} app={app}></AppCards>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
